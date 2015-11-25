2fa9ee6dbd48ed818d80c11bfdd1fc6952db117c
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-11-24 19:59:00 -0600

    Setup simple Bar Chart component with defaults.

--- demo/scripts/application.js
+++ demo/scripts/application.js
@@ -1,13 +1,20 @@
 var Diagrama    = require('./../../lib/index');
 var React       = require('react');
 var Block       = require('./Block');
+var render      = require('react-dom').render;
+
+var BarChart    = Diagrama.Bar;
+
 
 var DiagramaDemo = React.createClass({
     render: function() {
+        console.log(render);
         return (
-            <Block/>
+            <div>
+                <BarChart/>
+            </div>
         );
     }
 });
 
-React.render(<DiagramaDemo/>, document.getElementById('diagrama-demo'));
+render(<DiagramaDemo/>, document.getElementById('diagrama-demo'));
